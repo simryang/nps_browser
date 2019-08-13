73c80e1818a1a770e34dbdfa9377267192a713e4
simryang <239260+simryang@users.noreply.github.com>
2019-08-13 12:58:25 +0900

    Initial files from nopaystation.com/vita/npsReleases/

---
+++ NPS/Library.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace NPS
+{
+    partial class Library
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Testowy item",
+            "test",
+            "test2"}, "High-Definition-Ultra-HD-Wallpaper-96262544.jpg");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("test", "(none)");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Library));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2});
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(5, 29);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(929, 623);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "";
+            this.columnHeader1.Width = 600;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "High-Definition-Ultra-HD-Wallpaper-96262544.jpg");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Image = global::NPS.Properties.Resources.opened_folder;
+            this.button1.Location = new System.Drawing.Point(940, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open directory";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Image = global::NPS.Properties.Resources.menu_cancel;
+            this.button2.Location = new System.Drawing.Point(940, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 39);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Delete";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Enabled = false;
+            this.button3.Image = global::NPS.Properties.Resources.menu_unpack;
+            this.button3.Location = new System.Drawing.Point(940, 123);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 43);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Unpack PKG";
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Image = global::NPS.Properties.Resources.menu_reload;
+            this.button4.Location = new System.Drawing.Point(940, 193);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 43);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Refresh";
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Library
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 653);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Library";
+            this.Text = "Library";
+            this.Load += new System.EventHandler(this.Library_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ NPS/Library.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NPS
+{
+    public partial class Library : Form
+    {
+
+        List<Item> db;
+
+        public Library(List<Item> db)
+        {
+            InitializeComponent();
+            this.db = db;
+        }
+
+        private void Library_Load(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+
+            label1.Text = Settings.Instance.downloadDir;
+
+            string[] apps = new string[0];
+            string[] dlcs = new string[0];
+            string[] files = Directory.GetFiles(Settings.Instance.downloadDir, "*.pkg");
+
+            if (Directory.Exists(Settings.Instance.downloadDir + "\\packages"))
+            {
+                var lst = files.ToList();
+                lst.AddRange(Directory.GetFiles(Settings.Instance.downloadDir + "\\packages", "*.pkg"));
+                files = lst.ToArray();
+            }
+
+            if (Directory.Exists(Settings.Instance.downloadDir + "\\app"))
+            {
+                apps = Directory.GetDirectories(Settings.Instance.downloadDir + "\\app");
+            }
+            if (Directory.Exists(Settings.Instance.downloadDir + "\\addcont"))
+            {
+                dlcs = Directory.GetDirectories(Settings.Instance.downloadDir + "\\addcont");
+            }
+
+            List<string> imagesToLoad = new List<string>();
+
+            foreach (string s in files)
+            {
+                var f = Path.GetFileNameWithoutExtension(s);
+
+                bool found = false;
+                foreach (var itm in db)
+                {
+                    if (f.Equals(itm.DownloadFileName))
+                    {
+                        ListViewItem lvi = new ListViewItem(itm.TitleName + " (PKG)");
+
+                        listView1.Items.Add(lvi);
+
+                        foreach (var r in NPCache.I.renasceneCache)
+                            if (itm.Equals(r.itm))
+                            {
+                                imagesToLoad.Add(r.imgUrl);
+                                lvi.ImageKey = r.imgUrl;
+                                break;
+                            }
+                        LibraryItem library = new LibraryItem();
+                        library.itm = itm;
+                        library.path = s;
+                        library.isPkg = true;
+                        lvi.Tag = library;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    ListViewItem lvi = new ListViewItem(f + " (UNKNOWN PKG)");
+
+                    listView1.Items.Add(lvi);
+
+                    LibraryItem library = new LibraryItem();
+                    library.path = s;
+                    library.isPkg = true;
+                    lvi.Tag = library;
+                }
+            }
+
+            foreach (string s in apps)
+            {
+                string d = Path.GetFullPath(s).TrimEnd(Path.DirectorySeparatorChar).Split(Path.DirectorySeparatorChar).Last();
+
+                bool found = false;
+                foreach (var itm in db)
+                {
+                    if (!itm.IsDLC)
+                        if (itm.TitleId.Equals(d))
+                        {
+                            ListViewItem lvi = new ListViewItem(itm.TitleName);
+
+                            listView1.Items.Add(lvi);
+
+                            foreach (var r in NPCache.I.renasceneCache)
+                                if (itm.Equals(r.itm))
+                                {
+                                    imagesToLoad.Add(r.imgUrl);
+                                    lvi.ImageKey = r.imgUrl;
+                                    break;
+                                }
+                            LibraryItem library = new LibraryItem();
+                            library.itm = itm;
+                            library.path = s;
+                            library.isPkg = false;
+                            lvi.Tag = library;
+                            found = true;
+                            break;
+                        }
+                }
+
+                if (!found)
+                {
+                    ListViewItem lvi = new ListViewItem(d + " UNKNOWN");
+
+                    listView1.Items.Add(lvi);
+
+                    LibraryItem library = new LibraryItem();
+                    library.path = s;
+                    library.isPkg = false;
+                    lvi.Tag = library;
+                }
+            }
+
+
+
+            //foreach (string s in dlcs)
+            //{
+            //    string d = Path.GetFullPath(s).TrimEnd(Path.DirectorySeparatorChar).Split(Path.DirectorySeparatorChar).Last();
+            //    foreach (var itm in db)
+            //    {
+            //        if (itm.IsDLC && itm.TitleId.Equals(d))
+            //        {
+            //            ListViewItem lvi = new ListViewItem(itm.TitleName);
+
+            //            listView1.Items.Add(lvi);
+
+            //            foreach (var r in NPCache.I.renasceneCache)
+            //                if (itm == r.itm)
+            //                {
+            //                    imagesToLoad.Add(r.imgUrl);
+            //                    lvi.ImageKey = r.imgUrl;
+            //    break;
+            //                }
+            //            LibraryItem library = new LibraryItem();
+            //            library.itm = itm;
+            //            library.patch = s;
+            //            library.isPkg = false;
+            //            lvi.Tag = library;
+            //break;
+            //        }
+            //    }
+            //}
+
+
+            Task.Run(() =>
+            {
+                foreach (string url in imagesToLoad)
+                {
+                    WebClient wc = new WebClient();
+                    wc.Proxy = Settings.Instance.proxy;
+                    wc.Encoding = Encoding.UTF8;
+                    var img = wc.DownloadData(url);
+                    using (var ms = new MemoryStream(img))
+                    {
+                        Image image = Image.FromStream(ms);
+                        image = getThumb(image);
+                        Invoke(new Action(() =>
+                        {
+                            imageList1.Images.Add(url, image);
+                        }));
+                    }
+                }
+
+
+            });
+
+
+        }
+
+        public Bitmap getThumb(Image image)
+        {
+            int tw, th, tx, ty;
+            int w = image.Width;
+            int h = image.Height;
+            double whRatio = (double)w / h;
+
+            if (image.Width >= image.Height)
+            {
+                tw = 100;
+                th = (int)(tw / whRatio);
+            }
+            else
+            {
+                th = 100;
+                tw = (int)(th * whRatio);
+            }
+            tx = (100 - tw) / 2;
+            ty = (100 - th) / 2;
+            Bitmap thumb = new Bitmap(100, 100, PixelFormat.Format24bppRgb);
+            Graphics g = Graphics.FromImage(thumb);
+            g.Clear(Color.White);
+            g.InterpolationMode = InterpolationMode.NearestNeighbor;
+            g.DrawImage(image,
+            new Rectangle(tx, ty, tw, th),
+            new Rectangle(0, 0, w, h),
+            GraphicsUnit.Pixel);
+            return thumb;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0) return;
+            string path = (listView1.SelectedItems[0].Tag as LibraryItem).path;
+            System.Diagnostics.Process.Start("explorer.exe", "/select, " + path);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0) return;
+            var itm = (listView1.SelectedItems[0].Tag as LibraryItem); ;
+
+            try
+            {
+                if (itm.isPkg)
+                    File.Delete(itm.path);
+                else Directory.Delete(itm.path, true);
+
+                Library_Load(null, null);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0) return;
+            var itm = (listView1.SelectedItems[0].Tag as LibraryItem);
+            if (itm.isPkg == false) return;
+            if (itm.itm == null)
+            {
+                MessageBox.Show("Can't unpack unknown pkg");
+                return;
+            }
+
+            if (itm.itm.ItsPS3 && itm.path.ToLower().Contains("packages")) File.Move(itm.path, Settings.Instance.downloadDir + Path.DirectorySeparatorChar + Path.GetFileName(itm.path));
+
+            DownloadWorker dw = new DownloadWorker(itm.itm, this);
+            dw.Start();
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0) return;
+            var itm = (listView1.SelectedItems[0].Tag as LibraryItem);
+            button3.Enabled = itm.isPkg;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Library_Load(null, null);
+        }
+    }
+
+    class LibraryItem
+    {
+        public Item itm;
+        public bool isPkg = false;
+        public string path;
+    }
+}
